a0020c20c21fb22ea914155ffe74294e0e0d2d6c
tipikae <gilles.bd@free.fr>
2022-01-31 09:55:11 +0100

    adding auto-increment

--- src/main/java/com/tipikae/paymybuddy/entities/User.java
+++ src/main/java/com/tipikae/paymybuddy/entities/User.java
@@ -7,6 +7,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
@@ -35,6 +37,7 @@ public class User implements Serializable {
 	 * Id
 	 */
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 
