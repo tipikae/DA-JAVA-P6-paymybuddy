50b064bdbd3686df5f2458b765d0d9ccc79796e5
tipikae <gilles.bd@free.fr>
2022-01-31 14:28:14 +0100

    updating exceptions

--- src/main/java/com/tipikae/paymybuddy/exceptions/ConnectionForbiddenException.java
+++ src/main/java/com/tipikae/paymybuddy/exceptions/ConnectionForbiddenException.java
@@ -13,20 +13,7 @@ public class ConnectionForbiddenException extends PayMyBuddyException {
 
 	private static final long serialVersionUID = 1L;
 
-	public ConnectionForbiddenException() {
-		super();
-	}
-
-	public ConnectionForbiddenException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
 	public ConnectionForbiddenException(String message) {
 		super(message);
 	}
-
-	public ConnectionForbiddenException(Throwable cause) {
-		super(cause);
-	}
-
 }
--- src/main/java/com/tipikae/paymybuddy/exceptions/UserNotFoundException.java
+++ src/main/java/com/tipikae/paymybuddy/exceptions/UserNotFoundException.java
@@ -10,20 +10,7 @@ public class UserNotFoundException extends PayMyBuddyException {
 
 	private static final long serialVersionUID = 1L;
 
-	public UserNotFoundException() {
-		super();
-	}
-
-	public UserNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
 	public UserNotFoundException(String message) {
 		super(message);
 	}
-
-	public UserNotFoundException(Throwable cause) {
-		super(cause);
-	}
-
 }
