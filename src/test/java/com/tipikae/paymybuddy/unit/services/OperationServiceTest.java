6545ea3ce68dbd685c3da16d55e7d1ccb48fdd25
tipikae <gilles.bd@free.fr>
2022-02-07 19:07:26 +0100

    adding tests

--- src/test/java/com/tipikae/paymybuddy/unit/services/OperationServiceTest.java
+++ src/test/java/com/tipikae/paymybuddy/unit/services/OperationServiceTest.java
@@ -19,10 +19,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tipikae.paymybuddy.converters.IConverterListConnectionToConnectionDTO;
 import com.tipikae.paymybuddy.converters.IConverterPageOperationToOperationDTO;
 import com.tipikae.paymybuddy.dto.NewOperationDTO;
+import com.tipikae.paymybuddy.dto.NewTransferDTO;
 import com.tipikae.paymybuddy.dto.OperationDTO;
 import com.tipikae.paymybuddy.entities.Account;
 import com.tipikae.paymybuddy.entities.Operation;
@@ -34,6 +36,7 @@ import com.tipikae.paymybuddy.repositories.IOperationRepository;
 import com.tipikae.paymybuddy.services.IUserService;
 import com.tipikae.paymybuddy.services.OperationServiceImpl;
 
+@Transactional
 @ExtendWith(MockitoExtension.class)
 class OperationServiceTest {
 	
@@ -45,6 +48,8 @@ class OperationServiceTest {
 	private IConverterListConnectionToConnectionDTO converterConnectionToConnectionDTO;
 	@Mock
 	private IConverterPageOperationToOperationDTO converterOperationToOperationDTO;
+	@Mock
+	private BigDecimal rate;
 	
 	@InjectMocks
 	private OperationServiceImpl operationService;
@@ -134,6 +139,71 @@ class OperationServiceTest {
 		user.setAccount(account);
 		when(userService.isUserExists(anyString())).thenReturn(user);
 		assertThrows(OperationForbiddenException.class, () -> operationService.operation("bob@bob.com", operationDTO));
+	}
+	
+	@Test
+	void transferWhenOK() throws UserNotFoundException, OperationForbiddenException {
+		Account srcAccount = new Account();
+		srcAccount.setBalance(new BigDecimal(1000));
+		Account destAccount = new Account();
+		destAccount.setBalance(new BigDecimal(2000));
+		User alice = new User();
+		alice.setEmail("alice@alice.com");
+		alice.setAccount(srcAccount);
+		User bob = new User();
+		bob.setEmail("bob@bob.com");
+		bob.setAccount(destAccount);
+		NewTransferDTO transferDTO = new NewTransferDTO();
+		transferDTO.setAmount(new BigDecimal(500));
+		transferDTO.setDescription("test transfer");
+		transferDTO.setDestEmail(bob.getEmail());
+		when(userService.isUserExists(anyString())).thenReturn(alice, bob);
+		operationService.transfer(alice.getEmail(), transferDTO);
+		verify(operationRepository, Mockito.times(1)).save(any(Operation.class));
+	}
+	
+	@Test
+	void transferThrowsOperationForbiddenExceptionWhenEmailsEquals() {
+		String emailSrc = "alice@alice.com";
+		NewTransferDTO transferDTO = new NewTransferDTO();
+		transferDTO.setAmount(new BigDecimal(500));
+		transferDTO.setDescription("test transfer");
+		transferDTO.setDestEmail(emailSrc);
+		assertThrows(OperationForbiddenException.class, 
+				() -> operationService.transfer(emailSrc, transferDTO));
+	}
+	
+	@Test
+	void transferThrowsUserNotFoundExceptionWhenEmailNotFound() throws UserNotFoundException {
+		String emailSrc = "alice@alice.com";
+		NewTransferDTO transferDTO = new NewTransferDTO();
+		transferDTO.setAmount(new BigDecimal(500));
+		transferDTO.setDescription("test transfer");
+		transferDTO.setDestEmail("bob@bob.com");
+		when(userService.isUserExists(emailSrc)).thenThrow(UserNotFoundException.class);
+		assertThrows(UserNotFoundException.class, 
+				() -> operationService.transfer(emailSrc, transferDTO));
+	}
+	
+	@Test
+	void transferThrowsOperationForbiddenExceptionWhenAmountSupBalance() throws UserNotFoundException {
+		Account srcAccount = new Account();
+		srcAccount.setBalance(new BigDecimal(500));
+		Account destAccount = new Account();
+		destAccount.setBalance(new BigDecimal(2000));
+		User alice = new User();
+		alice.setEmail("alice@alice.com");
+		alice.setAccount(srcAccount);
+		User bob = new User();
+		bob.setEmail("bob@bob.com");
+		bob.setAccount(destAccount);
+		NewTransferDTO transferDTO = new NewTransferDTO();
+		transferDTO.setAmount(new BigDecimal(1000));
+		transferDTO.setDescription("test transfer");
+		transferDTO.setDestEmail(bob.getEmail());
+		when(userService.isUserExists(anyString())).thenReturn(alice, bob);
+		assertThrows(OperationForbiddenException.class, 
+				() -> operationService.transfer(alice.getEmail(), transferDTO));
 	}
 
 }
